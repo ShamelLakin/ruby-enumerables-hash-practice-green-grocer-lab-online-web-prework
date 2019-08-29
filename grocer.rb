baec76f592820660568e3f57ec4c213977ecf60d
Shamel Lakin <christlife4u@gmail.com>
2019-08-29 14:04:06 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -14,13 +14,13 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  bining.pry
 end
 
-def apply_clearance(cart)
-  # code here
-end
+# def apply_clearance(cart)
+#   # code here
+# end
 
-def checkout(cart, coupons)
-  # code here
-end
+# def checkout(cart, coupons)
+#   # code here
+# end
