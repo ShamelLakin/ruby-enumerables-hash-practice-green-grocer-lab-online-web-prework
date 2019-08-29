045788094300f1ed9216eef8700a53c2b69ac6ae
Shamel Lakin <christlife4u@gmail.com>
2019-08-29 14:01:46 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,16 @@
+require 'pry'
 def consolidate_cart(cart)
-  # code here
+  final_hash = {}
+  cart.each do |element_hash|
+    element_name = element_hash.keys[0]
+  
+    if final_hash.has_key?{element_name}
+      element_stats [:count] += 1
+    else
+      final_hash[element_name] = element_stats
+      final_hash[:count] = 1
+    end
+  end
 end
 
 def apply_coupons(cart, coupons)
